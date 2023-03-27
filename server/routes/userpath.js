6d322b3aca5ba62f29348f804d2fcc9e7c60dcfd
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-03-27 15:07:20 +0530

    Made changes to server routes

--- server/routes/userpath.js
+++ server/routes/userpath.js
@@ -27,12 +27,44 @@ router.get(`/getalluserposts`, fetchUser, async(req, res) => {
     }
 })
 
-router.post(`/createpost`, fetchUser, async(req, res) => {
+router.post(`/createpost`, fetchUser, [
+    body("image").exists(),
+], async(req, res) => {
+    try{
+        const {image, description}  = req.body
+        const errors = validationResult(req)
 
+        if (!errors.isEmpty())
+        {   
+            return res.status(400).json({errors: errors.array()})
+        }
+
+        const postIt = new Img({
+            userid: req.user._id,
+            image,
+            description, 
+        })
+        const savePost = await postIt.save()
+        res.json(savePost)
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send(`Internal Server Error`)
+    }
 })
 
 router.delete(`/deletepost/:id`, fetchUser, async(req, res) => {
+   try{
+    let postIt = await Img.findById(req.params.id)
+    if (!postIt)    res.status(404).send(`Not Found!`)
+    if (postIt.userid.toString() !== req.user.id)   res.status(401).send(`Not Allowed`)
 
+    postIt = await Img.findOneAndDelete(req.params.id)
+    res.json({"success": "deleted successfully!", postIt: postIt});
+   }catch(error){
+    console.log(error)
+    res.status(500).send(`Internal Server Error`)
+   }
 })
 
 module.exports = router
