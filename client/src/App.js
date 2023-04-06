39520b2fb308fd8ae57af973fc41bdebc0ecd455
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-04-06 15:34:43 +0530

    Modified client side structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Login  from "./components/login";
+import Signup  from "./components/signup";
+import Navbar from "./components/navbar";
+import Newnav  from './components/newnav';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  if (!localStorage.getItem('token')){
+    return (
+      <>
+        <Router>
+          <Navbar/>
+            <div className="App">
+              <Routes>
+                <Route exact path="/" element={<Login key="login" />}/>
+                <Route exact path="/signup" element={<Signup key="signup" />}/>
+              </Routes>
+            </div>
+        </Router>
+      </>
+    );
+  }else{
+    //think of the routes and the features to add
+    return(
+      <>
+      <Router>
+        <Newnav/>
+          <div className="App">
+            <Routes>
+            </Routes>
+          </div>
+      </Router>
+      </>
+    );
+  }
 }
 
 export default App;
